ebf7beae7daf2842beaa7363c893b8186a080a54
Nikita Semeniuk <nikstannum@yandex.ru>
2022-03-20 21:03:50 +0300

    create class DemoStrings

---
+++ src/com/belhard/string/DemoStrings.java
@@ -0,0 +1,8 @@
+package com.belhard.string;
+
+public class DemoStrings {
+    public static String[] stringsArray = {"camelCase, sixApples, iReallyLikeToLearnSomethingNew",
+            "Mike takes the BlAcK ball. Blackout curtains hang on the window",
+            "Andrew has 3 apples, and Lindsey has 265 apples. We have next values: 12, 1, 343, 9, 0, 11",
+            "This   line has  some whitespaces . ", "rotor"};
+}
--- src/com/belhard/string/Task1.java
+++ src/com/belhard/string/Task1.java
@@ -1,6 +1,6 @@
 package com.belhard.string;
 
-public class Main {
+public class Task1 {
 
     public static void main(String[] args) {
 
