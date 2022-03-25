95536ebf34faa502dc4e2e37e96db641ebbe1bdb
Nikita Semeniuk <nikstannum@yandex.ru>
2022-03-25 22:44:23 +0300

    Task4 complete

--- src/com/belhard/string/DemoStrings.java
+++ src/com/belhard/string/DemoStrings.java
@@ -3,8 +3,8 @@ package com.belhard.string;
 public class DemoStrings {
     public static String[] stringsArray = {"camelCase, apple, BICYCLE, BlackBoard, sixApples, iReallyLikeToLearnSomethingNew",
             "Mike takes the black ball and BlAcK boots. Blackout curtains hang on the window. findCamelCase, diGItalTEchnology, letterA",
-            "Andrew has 3 apples, and Lindsey has 265 apples. We have next values: 12,55.1, 1, 343, 9, 0, 11",
-            "This   line has  some whitespaces . ",
+            "Andrew has 3 apples, and Lindsey has 265 apples. We have next values: 12, 55.1, 1, 343, 9, 0, 11",
+            "This   line has  some whitespaces .  ",
             "rotor"};
 
     public static String[] getStringsArray() {
--- src/com/belhard/string/Task4.java
+++ src/com/belhard/string/Task4.java
@@ -1,4 +1,19 @@
 package com.belhard.string;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task4 {
+    public static void main(String[] args) {
+        String[] str = DemoStrings.getStringsArray();
+        Pattern regex = Pattern.compile("[\\d]+\\.[\\d]+ | [\\d]{2,}");
+        int counter = 0;
+        for (String s : str) {
+            Matcher matcher = regex.matcher(s);
+            while (matcher.find()) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
 }
