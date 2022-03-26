f02d0f9e046eb717a53b26e74ae869fa322d2bc2
Nikita Semeniuk <nikstannum@yandex.ru>
2022-03-26 22:47:07 +0300

    Task5 complete

--- src/com/belhard/string/DemoStrings.java
+++ src/com/belhard/string/DemoStrings.java
@@ -1,10 +1,10 @@
 package com.belhard.string;
 
 public class DemoStrings {
-    public static String[] stringsArray = {"camelCase, apple, BICYCLE, BlackBoard, sixApples, iReallyLikeToLearnSomethingNew",
+    public static final String[] stringsArray = {"camelCase, apple, BICYCLE, BlackBoard, sixApples, iReallyLikeToLearnSomethingNew",
             "Mike takes the black ball and BlAcK boots. Blackout curtains hang on the window. findCamelCase, diGItalTEchnology, letterA",
-            "Andrew has 3 apples, and Lindsey has 265 apples. We have next values: 12, 55.1, 1, 343, 9, 0, 11",
-            "This   line has  some whitespaces .  ",
+            "Andrew has 3 apples, and Lindsey has 265 apples. We have next values: 12, 55.1, 1, 343, 9, 0, 11. 20-th century",
+            "   This   line has  some whitespaces .  ",
             "rotor"};
 
     public static String[] getStringsArray() {
---
+++ src/com/belhard/string/Task5.java
@@ -0,0 +1,33 @@
+package com.belhard.string;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task5 {
+    public static void main(String[] args) {
+        String[] str = DemoStrings.getStringsArray();
+        MyCollection newArray = new MyDynamicArray();
+        Pattern regex = Pattern.compile("\\w\\s\\W");
+        for (int i = 0; i < str.length; i++) {
+            String[] arrayTrim = new String[str.length];
+            arrayTrim[i] = str[i].trim().replaceAll("\\s{2,}", " ");
+            Matcher matcher = regex.matcher(arrayTrim[i]);
+            if (matcher.find()) {
+                String group = matcher.group().replaceAll(" ", "");
+                String[] splitString = arrayTrim[i].split("\\w\\s\\W");
+                String withoutWhiteSpace = "";
+                for (String s : splitString) {
+                    withoutWhiteSpace = withoutWhiteSpace.concat(s);
+                }
+                withoutWhiteSpace = withoutWhiteSpace.concat(group);
+                newArray.add(withoutWhiteSpace);
+                continue;
+            }
+            newArray.add(arrayTrim[i]);
+
+        }
+        for (int i = 0; i < newArray.size(); i++) {
+            System.out.println(newArray.toArray()[i]);
+        }
+    }
+}
